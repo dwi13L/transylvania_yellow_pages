e5f8637388c9ed3165f280b1335fe1a8455cdc5c
dwi13L <dw22197@gmail.com>
2022-06-11 21:30:16 +0530

    Disabling StrictMode to avoid rendering twice

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
+
+/**
+ * Strict mode rendered component twice
+ * Further read
+ * https://stackoverflow.com/questions/49206509/child-component-constructor-called-multiple-times/61443443#61443443
+ */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
