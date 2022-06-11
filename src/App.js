6811a5921b158dd3e368c0e3027d4d354059db82
dwi13L <dw22197@gmail.com>
2022-06-11 19:41:25 +0530

    setState with functions explained

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">hello world</div>;
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      text: `hello`,
+    };
+  }
+
+  handleButtonClick = () => {
+    this.setState(
+      /**
+       * Access to current state is available as an argument to the functions
+       */
+      () => {
+        return { text: `Good bye` };
+      }, //updater function that returns an object that will be set as the state
+
+      (state) => {
+        console.log(state.text);
+      } //callback function that executes post async update
+    );
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <p> {this.state.text}</p>
+        <br />
+        <button onClick={this.handleButtonClick}>Leave</button>
+      </div>
+    );
+  }
+}
