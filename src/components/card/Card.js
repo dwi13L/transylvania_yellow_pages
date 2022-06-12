f50ebfebcfbb3d78e31e1786702595d35b51543e
dwi13L <dw22197@gmail.com>
2022-06-13 00:50:39 +0530

    Converted to functional components

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
 import "./Card.css";
 
-export default class Card extends Component {
+export default function Card({ item }) {
+  const { id, itemName, name, email, phone } = item;
+
+  return (
+    <div className="card">
+      <img
+        src={`https://robohash.org/${id}?set=set2`}
+        alt={`${itemName} ${name}`}
+      />
+      <h2 key={id}>{name}</h2>
+      <p>{phone}</p>
+      <p>{email}</p>
+    </div>
+  );
+}
+
+/**
+ * Class components ----------------------------------------------------------------------------------------
+ * @deprecated ported component to functional component
+ */
+export class _Card extends Component {
   render() {
     const { id, itemName, name, email, phone } = this.props.item;
     return (
